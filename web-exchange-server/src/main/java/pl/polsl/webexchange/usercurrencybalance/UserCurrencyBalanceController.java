24a2e951a1aea093a900043784ff1ce161586b7b
Krzysztof Sobocik <krzysob384@student.polsl.pl>
2021-01-23 12:36:38 +0100

    User Currency Balance API returns balance for all currencies, not only owned by user.

--- web-exchange-server/src/main/java/pl/polsl/webexchange/usercurrencybalance/UserCurrencyBalanceController.java
+++ web-exchange-server/src/main/java/pl/polsl/webexchange/usercurrencybalance/UserCurrencyBalanceController.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.polsl.webexchange.currency.Currency;
+import pl.polsl.webexchange.currency.CurrencyService;
 import pl.polsl.webexchange.user.User;
 
 import java.util.List;
@@ -15,12 +17,16 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class UserCurrencyBalanceController {
 
-    private final UserCurrencyBalanceRepository userCurrencyBalanceRepository;
+    private final UserCurrencyBalanceService userCurrencyBalanceService;
+    private final CurrencyService currencyService;
 
     @GetMapping("api/userBalance")
     public ResponseEntity<List<UserCurrencyBalanceDTO>> getUserCurrencyBalance(@AuthenticationPrincipal User user) {
-        List<UserCurrencyBalance> allByUser = userCurrencyBalanceRepository.findAllByUser(user);
-        List<UserCurrencyBalanceDTO> dtos = allByUser.stream().map(UserCurrencyBalanceDTO::new).collect(Collectors.toList());
+        List<Currency> allCurrencies = currencyService.getAllCurrencies();
+        List<UserCurrencyBalanceDTO> dtos = allCurrencies.stream()
+                .map(currency -> userCurrencyBalanceService.getUserCurrencyBalance(user, currency))
+                .map(UserCurrencyBalanceDTO::new)
+                .collect(Collectors.toList());
         return ResponseEntity.ok(dtos);
     }
 
--- web-exchange-server/src/main/java/pl/polsl/webexchange/usercurrencybalance/UserCurrencyBalanceRepository.java
+++ web-exchange-server/src/main/java/pl/polsl/webexchange/usercurrencybalance/UserCurrencyBalanceRepository.java
@@ -4,10 +4,8 @@ import org.springframework.data.repository.CrudRepository;
 import pl.polsl.webexchange.currency.Currency;
 import pl.polsl.webexchange.user.User;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserCurrencyBalanceRepository extends CrudRepository<UserCurrencyBalance, Long> {
-    List<UserCurrencyBalance> findAllByUser(User user);
     Optional<UserCurrencyBalance> findByUserAndCurrency(User user, Currency currency);
 }
