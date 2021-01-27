da90575edb2ba1b81b1696e9102d0ed506255d00
oladyr <oladyr123@gmail.com>
2021-01-27 10:32:47 +0100

    Fix graph.

--- web-exchange-frontend/src/app/currency-rates-graph/currency-rates-graph.component.ts
+++ web-exchange-frontend/src/app/currency-rates-graph/currency-rates-graph.component.ts
@@ -1,21 +1,23 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {GraphService} from '../service/graph.service';
 import {GraphSeries} from '../model/graph-series.model';
 import {CurrencyService} from '../service/currency.service';
 import {Currency} from '../model/currency.model';
-import {interval} from 'rxjs';
+import {interval, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-currency-rates-graph',
   templateUrl: './currency-rates-graph.component.html',
   styleUrls: ['./currency-rates-graph.component.scss']
 })
-export class CurrencyRatesGraphComponent implements OnInit {
+export class CurrencyRatesGraphComponent implements OnInit, OnDestroy {
   graphData: GraphSeries[] = [];
   currencies: Currency[] = [];
   selectedParams = {baseCurrency: '', targetCurrency: '', from: '', to: ''};
   displayedGraphParams = {baseCurrency: '', targetCurrency: '', from: '', to: ''};
   series: GraphSeries = {series: [], name: ''};
+
+  graphSubscription: Subscription = new Subscription();
 
   constructor(private graphService: GraphService,
               private currencyService: CurrencyService) {
@@ -25,6 +27,10 @@ export class CurrencyRatesGraphComponent implements OnInit {
     this.currencyService.getCurrencies().subscribe(currencies => {
       this.currencies = currencies;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.graphSubscription.unsubscribe();
   }
 
   fetchGraph(baseCurrency: string, targetCurrency: string, from: string, to: string) {
@@ -37,8 +43,9 @@ export class CurrencyRatesGraphComponent implements OnInit {
 
   fetchGraphInInterval() {
     this.fetchGraph(this.displayedGraphParams.baseCurrency, this.displayedGraphParams.targetCurrency, this.displayedGraphParams.from, this.displayedGraphParams.to);
+    this.graphSubscription.unsubscribe();
     const source = interval(10000);
-    source.subscribe(() => {
+    this.graphSubscription = source.subscribe(() => {
       this.fetchGraph(this.displayedGraphParams.baseCurrency, this.displayedGraphParams.targetCurrency, this.displayedGraphParams.from, this.displayedGraphParams.to);
     });
   }
